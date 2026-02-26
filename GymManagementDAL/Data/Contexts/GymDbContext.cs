0819e8be72225f1e0aa7aa9e64d69c84270c39a0
Jaser1010 <j.osama2101@nu.edu.eg>
2026-02-26 17:07:14 +0200

    membership

--- GymManagementDAL/Data/Contexts/GymDbContext.cs
+++ GymManagementDAL/Data/Contexts/GymDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using GymManagementDAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace GymManagementDAL.Data.Contexts
 {
-    public class GymDbContext : DbContext
+    public class GymDbContext : IdentityDbContext<ApplicationUser>
     {
         public GymDbContext(DbContextOptions<GymDbContext> options) : base(options)
         {
@@ -21,7 +23,14 @@ namespace GymManagementDAL.Data.Contexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.Entity<ApplicationUser>(Eb =>
+            {
+                Eb.Property(u => u.FirstName).HasColumnType("varchar").HasMaxLength(50);
+                Eb.Property(u => u.LastName).HasColumnType("varchar").HasMaxLength(50);
+			}
+            );
         }
 
         #region DbSets
